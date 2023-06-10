c35f3533d67cfc22f2aba8c9ed61743f575c969b
Victoria De Palma <victoriadepalma@Victorias-MacBook-Air.local>
2023-06-10 09:45:17 -0400

    First commit in Vicky

--- microproyecto2/src/App.jsx
+++ microproyecto2/src/App.jsx
@@ -3,33 +3,57 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    
+    <div className="container"> 
+    <section className="header"> 
+        <h1 className="title" id="maintitle"> SPOTIFINDER</h1> 
+        <h4 className="subtitle" id="subtitle">
+    Find your favorite artists and see their popular songs
+    </h4> 
+
+    <input
+        type="text" 
+        name= "query"
+        id="query"
+        className="search Input"
+        placeholder= "Busca tu artista favorito"
+    />
+
+        <button type= "button" className= "searchButton" id= "searchButton">
+        Buscar 
+        </button> 
+
+    <div id = "slide-0" className="slide-fade">
+      <img src="https://www.unimet.edu.ve/wp-content/uploads/2020/10/Campus-galer%C3%ADa-51.jpg" alt="img0"></img>
+    </div>
+    </section>
+
+{/* <section className= "input-container">
+    <input
+        type="text" 
+        name= "query"
+        id="query"
+        className="search Input"
+        placeholder= "Busca tu artista favorito"
+    />
+
+        <button type= "button" className= "searchButton" id= "searchButton">
+        Buscar 
+        </button> 
+
+</section> */}
+
+<section id="results" className="results"></section>
+
+</div>
+)
 }
+
 
 export default App
